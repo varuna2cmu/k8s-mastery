5ce9c9c7c33c35a58241991277f2e0e663ce1693
Varun Agarwal <agarwalvarun2000@gmail.com>
2024-02-23 18:31:23 -0500

    homework_3 changes
    Signed-off-by: Varun Agarwal <agarwalvarun2000@gmail.com>

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://34.133.158.137:80/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
